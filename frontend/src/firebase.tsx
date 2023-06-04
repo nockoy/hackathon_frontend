45a82b99f6ea47a7c8cc21270905abf18a70ac20
nockoy <clarinetbroken0819@gmail.com>
2023-06-04 22:56:39 +0900

    vercelに環境変数を設定

--- frontend/src/firebase.tsx
+++ frontend/src/firebase.tsx
@@ -1,11 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import {getStorage} from "firebase/storage"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
